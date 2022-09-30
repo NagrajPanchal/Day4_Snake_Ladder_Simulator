2d7cb54cb164362e017651c18c7da5818313057e
Nagraj <nagrajsubhashpanchal.gmail.com>
2022-09-30 23:25:43 +0530

    UC2 Snake and Ladder Simulator Roll die Random 1 to 6 numbers

--- src/com/bridgelabz/snakeladder/SnakeLadderSimulator.java
+++ src/com/bridgelabz/snakeladder/SnakeLadderSimulator.java
@@ -1,10 +1,12 @@
-// UC1 Snake and Ladder Simulator Game Starting Position 0
+// UC2 Player rolls the die to get a number between 1 to 6.
 
 package com.bridgelabz.snakeladder;
+import java.util.Random;
 public class SnakeLadderSimulator
 {
     static final int STARTPOINT = 0;
     public static int player1Position = 0;
+    static int dice = 0;
     void start()
     {
         if(STARTPOINT == player1Position)
@@ -12,9 +14,19 @@ public class SnakeLadderSimulator
             System.out.println("Start Position : " +STARTPOINT);
         }
     }
+    int rolldie()
+    {
+        int n = 0;
+        Random r = new Random();
+        n=r.nextInt(7);
+        return (n==0?1:n);
+    }
     public static void main(String[] args)
     {
         SnakeLadderSimulator sls = new SnakeLadderSimulator();
         sls.start();
+        dice = sls.rolldie();
+        System.out.println(dice);
+
     }
 }
