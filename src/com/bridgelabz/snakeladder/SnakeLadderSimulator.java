76a037147827494d1bf177cf77bc516b8d63913a
Nagraj <nagrajsubhashpanchal.gmail.com>
2022-10-01 00:58:19 +0530

    UC3 Player checks for a Option, They are No Play, Ladder or Snake

--- src/com/bridgelabz/snakeladder/SnakeLadderSimulator.java
+++ src/com/bridgelabz/snakeladder/SnakeLadderSimulator.java
@@ -1,4 +1,4 @@
-// UC2 Player rolls the die to get a number between 1 to 6.
+// UC3 Player checks for a Option, They are No Play, Ladder or Snake.
 
 package com.bridgelabz.snakeladder;
 import java.util.Random;
@@ -7,6 +7,7 @@ public class SnakeLadderSimulator
     static final int STARTPOINT = 0;
     public static int player1Position = 0;
     static int dice = 0;
+
     void start()
     {
         if(STARTPOINT == player1Position)
@@ -14,19 +15,71 @@ public class SnakeLadderSimulator
             System.out.println("Start Position : " +STARTPOINT);
         }
     }
-    int rolldie()
+    static int rolldie()
     {
         int n = 0;
         Random r = new Random();
         n=r.nextInt(7);
         return (n==0?1:n);
     }
+    static void snake()
+    {
+        if(player1Position == 33)
+        {
+            player1Position = 3;
+        }
+        if(player1Position == 93)
+        {
+            player1Position = 38;
+        }
+        if(player1Position == 77)
+        {
+            player1Position = 18;
+        }
+        if(player1Position == 67)
+        {
+            player1Position = 23;
+        }
+    }
+    static void ladder()
+    {
+        if(player1Position == 5)
+        {
+            player1Position = 48;
+        }
+        if(player1Position == 11)
+        {
+            player1Position = 58;
+        }
+        if(player1Position == 37)
+        {
+            player1Position = 95;
+        }
+        if(player1Position == 25)
+        {
+            player1Position = 75;
+        }
+    }
+    static void noPlay()
+    {
+        if(dice != 0)
+        {
+            player1Position = player1Position*1;
+        }
+    }
+    static void calculatePlayerPosition()
+    {
+        dice = rolldie();
+        player1Position = player1Position + dice;
+        SnakeLadderSimulator.noPlay();
+        SnakeLadderSimulator.snake();
+        SnakeLadderSimulator.ladder();
+        System.out.println(player1Position);
+    }
     public static void main(String[] args)
     {
         SnakeLadderSimulator sls = new SnakeLadderSimulator();
         sls.start();
-        dice = sls.rolldie();
-        System.out.println(dice);
-
+        sls.calculatePlayerPosition();
     }
 }
