e29a9de0022913c433f5b556e91a8aaf0b5c99d0
Nagraj <nagrajsubhashpanchal.gmail.com>
2022-10-02 04:52:41 +0530

    UC6 Report the number of times the dice was played to win the game and also the position after every die role

--- src/com/bridgelabz/snakeladder/SnakeLadderSimulator.java
+++ src/com/bridgelabz/snakeladder/SnakeLadderSimulator.java
@@ -1,12 +1,15 @@
-// UC4 Repeat till the Player reaches the winning position 100.
-
+/* UC6 Report the number of times the dice was played to win the game and
+ also the position after every die role
+*/
 package com.bridgelabz.snakeladder;
 import java.util.Random;
 public class SnakeLadderSimulator
 {
-    static final int STARTPOINT = 0;
+    public static final int STARTPOINT = 0;
+    public static final int WINNINGPOINT = 100;
     public static int player1Position = 0;
     static int dice = 0;
+    public int count = 0;
 
     void start()
     {
@@ -62,27 +65,45 @@ public class SnakeLadderSimulator
     }
     static void noPlay()
     {
-        if(dice != 0)
+        if(player1Position > WINNINGPOINT)
         {
-            player1Position = player1Position*1;
+            player1Position = player1Position - dice;
+        }
+    }
+    static void setWinningPoint()
+    {
+        if (player1Position > WINNINGPOINT)
+        {
+            player1Position = player1Position - dice;
+        }
+    }
+    static void win()
+    {
+        if(player1Position == WINNINGPOINT)
+        {
+            System.out.println("Congratulation 'Player1' won the Snake and Ladder Game ");
         }
     }
     static void calculatePlayerPosition()
     {
         dice = rolldie();
+        SnakeLadderSimulator.setWinningPoint();
         player1Position = player1Position + dice;
         SnakeLadderSimulator.noPlay();
         SnakeLadderSimulator.snake();
         SnakeLadderSimulator.ladder();
         System.out.println(player1Position);
+        SnakeLadderSimulator.win();
     }
     public static void main(String[] args)
     {
         SnakeLadderSimulator sls = new SnakeLadderSimulator();
-        for (player1Position = 0; player1Position <100; player1Position++)
+        for (player1Position = 0; player1Position <= WINNINGPOINT; player1Position++)
         {
             sls.start();
             sls.calculatePlayerPosition();
+            sls.count++;
         }
+        System.out.println("Number of times dice was played : "+sls.count);
     }
 }
